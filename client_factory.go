00071532285c90c53c555a54ae655ebdb201a3ea
dengyi <dengyi@didichuxing.com>
2018-01-06 17:39:50 +0800

    add client factory

---
+++ client_factory.go
@@ -0,0 +1,66 @@
+package marathon
+
+import (
+    "sync"
+
+    "github.com/nienie/marathon/loadbalancer"
+    "github.com/nienie/marathon/http"
+)
+
+var (
+    cf *clientFactory
+)
+
+func init() {
+    cf = newClientFactory()
+}
+
+type clientFactory struct {
+    loadBalancers    map[string]loadbalancer.LoadBalancer
+    lbLock           sync.Mutex
+    clients          map[string]*httpclient.LoadBalancerHTTPClient
+    clientLock       sync.Mutex
+}
+
+func newClientFactory() *clientFactory {
+    return &clientFactory{
+        loadBalancers:      make(map[string]loadbalancer.LoadBalancer),
+        lbLock:             sync.Mutex{},
+        clients:            make(map[string]*httpclient.LoadBalancerHTTPClient),
+        clientLock:         sync.Mutex{},
+    }
+}
+
+func RegisterLoadBalancer(name string, lb loadbalancer.LoadBalancer) {
+    if len(name) == 0 || lb == nil {
+        return
+    }
+    if _, ok := cf.loadBalancers[name]; ok {
+        return
+    }
+    cf.lbLock.Lock()
+    cf.loadBalancers[name] = lb
+    cf.lbLock.Unlock()
+}
+
+func RegisterHTTPClient(name string, client *httpclient.LoadBalancerHTTPClient) {
+    if len(name) == 0 || client == nil {
+        return
+    }
+    if _, ok := cf.clients[name]; ok {
+        return
+    }
+    cf.clientLock.Lock()
+    cf.clients[name] = client
+    cf.clientLock.Unlock()
+}
+
+//GetLoadBalancer ...
+func GetLoadBalancer(name string) loadbalancer.LoadBalancer {
+    return cf.loadBalancers[name]
+}
+
+//GetHTTPClient ...
+func GetHTTPClient(name string) *httpclient.LoadBalancerHTTPClient {
+    return cf.clients[name]
+}
