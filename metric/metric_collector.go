0c1f677fb7bca2329998004395a990446a0c66b3
dengyi <dengyi@didichuxing.com>
2018-01-08 21:52:24 +0800

    add metric package

---
+++ metric/metric_collector.go
@@ -0,0 +1,15 @@
+package metric
+
+import (
+    "context"
+    "time"
+
+    "github.com/nienie/marathon/client"
+)
+
+//Collector ...
+type Collector interface {
+
+    //RPC ...
+    RPC(context.Context, client.Request, client.Response, error, time.Duration)
+}
